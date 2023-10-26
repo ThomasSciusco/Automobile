4e52e20ec2b6eedabcc84d590a474c54293d050a
sciusco.20197 <sciusco.20197@E2-9-PC18>
2023-10-26 10:40:02 +0200

    Aggiungere i file di progetto.

---
+++ automobile/macchina.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace automobile
+{
+    public class amacchina
+    {
+        private bool MotoreOn = false;
+        private int velocita = 0;
+        private int marcia = 0;
+
+        public void AccendiSpegni()
+        {
+            this.MotoreOn = MotoreOn;
+        }
+        public void accelerare()
+        {
+            if (MotoreOn && marcia < 5)
+            {
+                velocita += 10;
+            }
+        }
+        public void decelerare()
+        {
+
+            if (MotoreOn && velocita > 0)
+            {
+                velocita -= 10;
+            }
+        }
+        public void aumentamarcia()
+        {
+            if (MotoreOn && marcia < 5)
+            {
+                marcia++;
+            }
+        }
+        public void diminuiremarcia()
+        {
+            if (MotoreOn && marcia > 0)
+            {
+                marcia--;
+            }
+        }
+
+        public int Getvelocita()
+        {
+            return velocita;
+        }
+
+        public int Getmarcia()
+        {
+            return marcia;
+        }
+    }
+}
+
+
